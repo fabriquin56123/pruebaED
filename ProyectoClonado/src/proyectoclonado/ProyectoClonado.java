17b95e0a36caddd047d00e89c26334b5f7a62481
fabriquin56123 <fabrizzio.quintana@unmsm.edu.pe>
2022-07-24 10:27:35 -0500

    cambios mamamamama

--- ProyectoClonado/src/proyectoclonado/ProyectoClonado.java
+++ ProyectoClonado/src/proyectoclonado/ProyectoClonado.java
@@ -16,6 +16,8 @@ public class ProyectoClonado {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Cosme god");
+        String hola ="bb";
     }
     
 }
